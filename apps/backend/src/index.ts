d94720c7f9039e18caf54d8e57f3253acf9a433e
pjsk02 <pulipati.j@northeastern.edu>
2026-05-31 19:05:49 -0400

    chore: update index.ts

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -6,6 +6,7 @@ import { groupsRouter } from './routes/groups';
 import { membersRouter } from './routes/members';
 import { budgetRouter } from './routes/budget';
 import { itineraryRouter } from './routes/itinerary';
+import { evalRouter } from './routes/eval';
 import { initWeave } from './lib/weave';
 
 const app = express();
